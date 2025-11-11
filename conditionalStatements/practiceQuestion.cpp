27a5b35f882c72bec7c5ce54f4baff71b1f5e5c5
abhinav kumar <abhinavkashyap702@gmail.com>
2025-11-11 12:45:54 +0530

    Done Question 3

--- conditionalStatements/practiceQuestion.cpp
+++ conditionalStatements/practiceQuestion.cpp
@@ -4,44 +4,26 @@ using namespace std;
 int main()
 {
 
-    // Q1. Even or Odd
-    // Write a program to check whether a number entered by the user is even or odd using if-else.
 
-    int number;
-    cout << "Enter number for cheack wheather number is even or odd: ";
-    cin >> number;
-
-    if (number%2==0){
-        cout << "This number " << number <<  " is Even number" << "\n";
-    }else{
-        cout << "This number " << number <<  " is Odd number" << "\n";
-
-    }
-
-    // Q2. Maximum of Two Numbers
-    // Take two numbers from the user and find the greater number using the ternary operator (?:).
-       
-    int number1;
-    int number2;
-
-    cout << "Enter first number: ";
-    cin >> number1;
-
-    cout << "Enter second number: ";
-    cin >> number2;
-
-    // int greatestNumber = (number1 > number2)?  number1 : number2 ;
-    // cout << "greatest number is " << greatestNumber <<endl;   - if the value is the same datatype then it will store 
-
-    // if the data will not in both type then it will not store print it 
-
-    (number1 > number2)?
-                        cout << "The greatest number between " << number1 << " and " << number2 << " is: " << number1
-                        : cout << "Greatest number betweeen " << number1 << " and " << number2 << " is: " << number2 << "\n"; 
-                                                                       
 
     // Q3. Positive, Negative, or Zero
     // Check whether a number is positive, negative, or zero using if-else-if.
+
+    int num;
+
+    cout << "Enter number of cheack positive negative or Zero: ";
+    cin >> num;
+
+    if (num == 0){
+        cout << "This number is Zero: " << num << endl;
+    }
+    else if (num > 0){
+        cout << "This number is postive number: " << num << endl;
+    }
+    else{
+        cout << "This number is negative number: " << num << endl;
+    }
+    
 
     // Q4. Voting Eligibility
     // Take a person’s age as input and check if they are eligible to vote (age ≥ 18).
