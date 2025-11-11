c90c39fb6cdf5a8600b50749f85695ed32b24605
abhinav kumar <abhinavkashyap702@gmail.com>
2025-11-11 10:25:38 +0530

    sum of two numbers

---
+++ operator/practice.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //@1 sum of two number
+
+    int  num1;
+    int num2;
+
+    cout  << "Enter first number number: ";
+    cin >> num1;
+
+    cout  << "Enter second number number: " ;
+    cin >> num2;
+
+    int result = num1 + num2;
+    cout << "Sum of number " << num1 << "and " << num2 << "is " << result << "\n";
+
+
+
+    
+
+    return 0;
+}
