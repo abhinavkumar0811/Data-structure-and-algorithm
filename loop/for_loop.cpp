357357684cd805f9147d00e3b4efd390d21c62c1
abhinav kumar <abhinavkashyap702@gmail.com>
2025-11-14 11:45:24 +0530

    solved @1 to Q7

--- loop/for_loop.cpp
+++ loop/for_loop.cpp
@@ -46,6 +46,125 @@ int main(){
 
 
 
+//     FOR LOOP — 10 Practical Questions
+
+// Q2 Print numbers from 1 to 10
+// ➤ Output: 1 2 3 4 5 6 7 8 9 10
+for (int i = 0; i <=10; i++){
+    cout << i << endl;
+}
+
+// Q2 Print the sum of the first N natural numbers
+// ➤ Input: N = 5 → Output: 15
+
+
+int number;
+
+cout << "Enter number: ";
+cin >> number;
+
+    int sum = 0;
+    int idx = 1;
+    for (; idx <=number; idx++){
+        sum += idx;
+    }
+
+cout << "Sum of n natural number is: " << sum << endl;
+
+
+// Q3 Print all even numbers between 1 to 50
+
+int number;
+
+cout << "Enter number: ";
+cin >> number;
+
+    for (int counter=0; counter <=50; counter++){
+        if(counter%2==0){
+            cout << counter << endl;
+        }
+    }
+
+// Q4 Print the multiplication table of a given number
+// ➤ Input: 5
+// ➤ Output: 5 10 15 20 25 30 35 40 45 50
+
+int number;
+
+cout << "Enter number: ";
+cin >> number;
+
+int first_itrate = 1;
+int last_itrate = 10;
+
+    for(; first_itrate <= last_itrate;  first_itrate++ ){
+        cout << number << "x" << first_itrate << "=" << number*first_itrate << endl;
+    }
+
+
+// Q5 Find the factorial of a number using for loop
+// ➤ Input: 5 → Output: 120
+
+int number;
+int factorial = 1;
+
+cout << "Enter number: ";
+cin >> number;
+
+    for (int num=1;  num<=number;  num++){
+        factorial = factorial * num;
+    }
+
+    cout << "factorial of number: " << number<< "is: " << factorial << endl;
+
+
+
+// Q6 Print the reverse of a number
+// ➤ Input: 1234 → Output: 4321
+
+int  reversedNumber;
+
+cout << "Enter number: ";
+cin >> reversedNumber;
+
+    int idx =1;
+    for (;  idx <=reversedNumber;  reversedNumber--){
+        cout << reversedNumber << endl;
+    }
+
+    
+
+// Q6 Print Fibonacci series up to N terms
+// ➤ Input: 6 → Output: 0 1 1 2 3 5
+
+
+// Q7 Find the sum of digits of a number
+// ➤ Input: 1234 → Output: 10
+
+
+// int number;
+
+// cout << "Enter number: ";
+// cin >> number;
+
+//     int sum = 0;
+//     int idx = 1;
+//     for (; idx <=number; idx++){
+//         sum += idx;
+//     }
+
+// cout << "Sum of n natural number is: " << sum << endl;
+
+
+// @8 Check if a number is prime or not using for loop
+
+
+
+
+
+
+
+
 return 0;
 
 
--- solve.cpp
+++ solve.cpp
@@ -5,32 +5,43 @@ int main() {
     
 
 
-// Q8. Character Type Checker
-// Input a character and check whether it is:
-// An alphabet,    
-// A digit, or
-// A special character.
-// Use if-else-if.
+// Print the sum of the first N natural numbers
+// ➤ Input: N = 5 → Output: 15
+
+int number;
+
+cout << "Enter number: ";
+cin >> number;
 
 
-
-cout << "Cheacker" << endl;
-
-char charctor;
-
-cout << "Enter your charactor: ";
-cin >> charctor;
-
-if ((charctor >= "a" && charctor <= "z")){
-    cout << "This is a alphabet charactor" << endl;
+int idx = 1;
+int sum = 0;
+for (; idx <= number; idx++){
+    sum += idx;
 }
-else if (){
-    
-}
-else{
 
-    cout << "This is a special charactor" << endl;
-}
+cout << "Sum of n natural number is: " << sum << endl;
+// Print all even numbers between 1 to 50
+
+// Print the multiplication table of a given number
+// ➤ Input: 5
+// ➤ Output: 5 10 15 20 25 30 35 40 45 50
+
+// Find the factorial of a number using for loop
+// ➤ Input: 5 → Output: 120
+
+// Print the reverse of a number
+// ➤ Input: 1234 → Output: 4321
+
+// Print Fibonacci series up to N terms
+// ➤ Input: 6 → Output: 0 1 1 2 3 5
+
+// Find the sum of digits of a number
+// ➤ Input: 1234 → Output: 10
+
+// Check if a number is prime or not using for loop
+
+
 
 
     return 0;
